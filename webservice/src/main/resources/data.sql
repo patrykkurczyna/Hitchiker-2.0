7bc16ad936800dc1dc95624d3342aea5612c01db
patrykkurczyna <patryk.kurczyna@schibsted.pl>
2014-06-02 23:02:18 +0200

    Changing sample data

--- webservice/src/main/resources/data.sql
+++ webservice/src/main/resources/data.sql
@@ -1,1 +1,1 @@
-insert into hitchhiker (number_of_passengers, baggage, sex_type, age_type, children, destination, firstname, lastname, geo_latitude, geo_longitude) values (3, 'BIG', 'MIXED', 'YOUNG', false, 'Kraków', 'Jan', 'Resiak', 50.085627, 19.9736359); 
+insert into hitchhiker (number_of_passengers, baggage, sex_type, age_type, children, destination, firstname, lastname, geo_latitude, geo_longitude) values (3, 'BIG', 'MIXED', 'YOUNG', false, 'Kraków', 'Jan', 'Kowalski', 50.085627, 19.9736359); 
