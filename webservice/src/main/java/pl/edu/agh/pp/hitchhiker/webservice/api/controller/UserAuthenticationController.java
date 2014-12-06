34b7eb29c22dbae2ac25c2128b55fec014b68903
patrykkurczyna <patryk.kurczyna@schibsted.pl>
2014-12-06 14:39:03 +0100

    Changed response status to 417 when credentials are invalid

--- webservice/src/main/java/pl/edu/agh/pp/hitchhiker/webservice/api/controller/UserAuthenticationController.java
+++ webservice/src/main/java/pl/edu/agh/pp/hitchhiker/webservice/api/controller/UserAuthenticationController.java
@@ -1,9 +1,9 @@
 package pl.edu.agh.pp.hitchhiker.webservice.api.controller;
 
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.Resource;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -17,8 +17,6 @@ import pl.edu.agh.pp.hitchhiker.webservice.api.ApiUtil;
 import pl.edu.agh.pp.hitchhiker.webservice.model.AuthenticationCredentials;
 import pl.edu.agh.pp.hitchhiker.webservice.model.User;
 import pl.edu.agh.pp.hitchhiker.webservice.repository.UserRepository;
-
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 /**
  * Controller class for getting user authentication info
@@ -38,20 +36,19 @@ public class UserAuthenticationController {
 	 * @return Boolean entity, true when login and password are valid, false otherwise
 	 */
 	@RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-	HttpEntity<Resource<Boolean>> authenticateUser(@RequestBody AuthenticationCredentials credentials) {
+	ResponseEntity<String> authenticateUser(@RequestBody AuthenticationCredentials credentials) {
 		User user = userRepository.findByLogin(credentials.getLogin());
 		
 		if (user != null) {	
 			Boolean passwordIsCorrect = user.matches(credentials.getPassword());
-			Resource<Boolean> resource = new Resource<Boolean>(passwordIsCorrect);
-			
-			HttpHeaders headers = ApiUtil.createHeaders();
-			StringBuilder builder = new StringBuilder(linkTo(UserAuthenticationController.class).toString().replaceFirst("authenticateUser", "users"));
-			String userURL = builder.append("/").append(user.getId()).toString();
-			headers.add("Location", userURL);
-			
-			return new ResponseEntity<Resource<Boolean>>(resource, headers, HttpStatus.OK);
+			if (passwordIsCorrect) {
+				HttpHeaders headers = ApiUtil.createHeaders();
+				StringBuilder builder = new StringBuilder(linkTo(UserAuthenticationController.class).toString().replaceFirst("authenticateUser", "users"));
+				String userURL = builder.append("/").append(user.getId()).toString();
+				headers.add("Location", userURL);
+				return new ResponseEntity<String>(headers, HttpStatus.OK);				
+			}
 		}
-		return new ResponseEntity<Resource<Boolean>>(HttpStatus.NO_CONTENT);
+		return new ResponseEntity<String>(HttpStatus.EXPECTATION_FAILED);
 	}
 }
