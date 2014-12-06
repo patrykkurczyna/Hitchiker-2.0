22a919b86528eb7d44bbd70ff9ac140809438c1e
patrykkurczyna <patryk.kurczyna@schibsted.pl>
2014-12-06 18:52:29 +0100

    Handling sending notifications to hitchhikers that are inactive

--- webservice/src/main/java/pl/edu/agh/pp/hitchhiker/webservice/api/controller/DriverToHitchhikerActionsController.java
+++ webservice/src/main/java/pl/edu/agh/pp/hitchhiker/webservice/api/controller/DriverToHitchhikerActionsController.java
@@ -6,9 +6,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import pl.edu.agh.pp.hitchhiker.service.gcm.SendingNotificationsService;
+import pl.edu.agh.pp.hitchhiker.webservice.model.Driver;
+import pl.edu.agh.pp.hitchhiker.webservice.model.Hitchhiker;
 import pl.edu.agh.pp.hitchhiker.webservice.repository.DriverRepository;
 import pl.edu.agh.pp.hitchhiker.webservice.repository.HitchhikerRepository;
 
@@ -36,9 +37,13 @@ public class DriverToHitchhikerActionsController {
 	 * @param driverId id of {@link Driver} who sends notification
 	 */
 	@RequestMapping(value = "/driverWantsToTakeHitch", method = RequestMethod.GET)
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	void driverWantsToTakeHitch(@RequestParam("hitchId") final Integer hitchId, @RequestParam("driverId") final Integer driverId) {
-		service.sendDriverWantsToTakeYou(driverRepository.findById(driverId), hitchhikerRepository.findById(hitchId));
+	HttpStatus driverWantsToTakeHitch(@RequestParam("hitchId") final Integer hitchId, @RequestParam("driverId") final Integer driverId) {
+		Hitchhiker hitch = hitchhikerRepository.findById(hitchId);
+		if (hitch != null && hitch.isActive())  {
+			service.sendDriverWantsToTakeYou(driverRepository.findById(driverId), hitch);
+			return HttpStatus.ACCEPTED;
+		}
+		return HttpStatus.EXPECTATION_FAILED;
 	}
 	
 	/**
@@ -48,8 +53,12 @@ public class DriverToHitchhikerActionsController {
 	 * @param driverId id of {@link Driver} who sends notification
 	 */
 	@RequestMapping(value = "/driverTookHitch", method = RequestMethod.GET)
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	void driverTookHitch(@RequestParam("hitchId") final Integer hitchId, @RequestParam("driverId") final Integer driverId) {
-		service.sendDriverTookYou(driverRepository.findById(driverId), hitchhikerRepository.findById(hitchId));
+	HttpStatus driverTookHitch(@RequestParam("hitchId") final Integer hitchId, @RequestParam("driverId") final Integer driverId) {
+		Hitchhiker hitch = hitchhikerRepository.findById(hitchId);
+		if (hitch != null && hitch.isActive())  {
+			service.sendDriverTookYou(driverRepository.findById(driverId), hitch);
+			return HttpStatus.ACCEPTED;
+		}
+		return HttpStatus.EXPECTATION_FAILED;
 	}
 }
