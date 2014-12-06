6180af1e5365e991d63b5bc64854c7309a229fbb
patrykkurczyna <patryk.kurczyna@schibsted.pl>
2014-12-06 14:19:53 +0100

    Returning 204 when user is null

--- webservice/src/main/java/pl/edu/agh/pp/hitchhiker/webservice/api/controller/UserAuthenticationController.java
+++ webservice/src/main/java/pl/edu/agh/pp/hitchhiker/webservice/api/controller/UserAuthenticationController.java
@@ -41,14 +41,17 @@ public class UserAuthenticationController {
 	HttpEntity<Resource<Boolean>> authenticateUser(@RequestBody AuthenticationCredentials credentials) {
 		User user = userRepository.findByLogin(credentials.getLogin());
 		
-		Boolean passwordIsCorrect = user.matches(credentials.getPassword());
-		Resource<Boolean> resource = new Resource<Boolean>(passwordIsCorrect);
-		
-		HttpHeaders headers = ApiUtil.createHeaders();
-		StringBuilder builder = new StringBuilder(linkTo(UserAuthenticationController.class).toString().replaceFirst("authenticateUser", "users"));
-		String userURL = builder.append("/").append(user.getId()).toString();
-		headers.add("Location", userURL);
-		
-		return new ResponseEntity<Resource<Boolean>>(resource, headers, HttpStatus.OK);
+		if (user != null) {	
+			Boolean passwordIsCorrect = user.matches(credentials.getPassword());
+			Resource<Boolean> resource = new Resource<Boolean>(passwordIsCorrect);
+			
+			HttpHeaders headers = ApiUtil.createHeaders();
+			StringBuilder builder = new StringBuilder(linkTo(UserAuthenticationController.class).toString().replaceFirst("authenticateUser", "users"));
+			String userURL = builder.append("/").append(user.getId()).toString();
+			headers.add("Location", userURL);
+			
+			return new ResponseEntity<Resource<Boolean>>(resource, headers, HttpStatus.OK);
+		}
+		return new ResponseEntity<Resource<Boolean>>(HttpStatus.NO_CONTENT);
 	}
 }
